b0499caca3148c3398360acc5db4b9b2035c885e
Jamie Reid <wjreid85.jr@gmail.com>
2018-12-06 14:38:24 +0000

    Add initial files, create tables, create movie class.

---
+++ models/movie.rb
@@ -0,0 +1,20 @@
+
+require_relative('../db/sql_runner')
+
+class Movie
+
+  attr_accessor :title, :genre
+  attr_reader :id
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @title = options['title']
+    @genre = options['genre']
+  end
+
+
+
+
+
+
+end
